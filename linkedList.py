dd94d5f5929a80818e489d8d490df76c7ec974b2
Marco Arias <marcoarcha@hotmail.com>
2024-06-23 08:50:38 -0600

    remove_at and insert_at implementation

--- linkedList.py
+++ linkedList.py
@@ -46,8 +46,52 @@ class LinkedList:
             count += 1
             itr = itr.next
         return count
+    
+    def remove_at(self, index):
+        if index < 0 or self.get_lenght() <= index:
+            raise Exception("Invalid index")
+        
+        if index == 0:
+            self.head = self.head.next
+            return
+        
+        count = 0
+
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+
+            itr = itr.next
+            count += 1
+    def insert_at(self, index, data):
+        if index <0 or index > self.get_lenght():
+            raise Exception("Invalid Index")
+    
+        if index == 0:
+            self.insert_at_begining(data)
+            return
+        
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+
+            itr = itr.next
+            count += 1
+
+
+
+
+
 if __name__ == '__main__':
 
     ll = LinkedList()
     ll.insert_values(["banana", "mango", "grapes", "orange",])
+    ll.print()
+    ll.insert_at(2, "pineapple")
     ll.print()
